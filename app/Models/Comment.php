cb1e0ba1b7b4f5b542037e74d138385d687d12bf
saman <138027864+Sa-arash@users.noreply.github.com>
2025-01-31 10:21:47 +0330

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    /** @use HasFactory<\Database\Factories\CommentFactory> */
-    use HasFactory;
+    
+    use HasFactory, SoftDeletes;
+    protected $guarded = ['id'];
+    protected $fillable = [
+        'reply',
+        'user_id',
+        'product_id',
+        'comment',
+        'star',
+        'is_show',
+    ];
+
+    protected $casts = [
+        'reply' => 'array', 
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
