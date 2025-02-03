80a5e4f0c9003a6f74bed15d56c6162e24dc948f
SAJDEV <125678555+SAJDEV-s@users.noreply.github.com>
2025-02-03 21:13:12 +0330

    add page and widget

---
+++ app/Filament/Widgets/TableStatus.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Enums\FreeUse;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class TableStatus extends BaseWidget
+{
+    protected int | string | array $columnSpan='full';
+
+
+    public function table(Table $table): Table
+    {
+        return $table->emptyStateHeading('هیچ میز درحال استفاده ای نیست')->heading('میز های درحال استفاده')
+            ->query(
+                \App\Models\Table::query()->where('status','use')
+            )
+            ->columns([
+                Tables\Columns\TextColumn::make('')->rowIndex(),
+                Tables\Columns\TextColumn::make('title')->label('عنوان')->searchable(),
+                Tables\Columns\TextColumn::make('code')->label('کد میز')->searchable(),
+                Tables\Columns\TextColumn::make('status')->badge()->label('وضعیت'),
+                Tables\Columns\TextColumn::make('description')->label('توضیحات'),
+
+            ])->actions([
+                Tables\Actions\Action::make('change')->label('تغییر وضعیت میز')->action(function ($record){
+                    $record->update(['status'=>'free']);
+                })->requiresConfirmation()->modalSubmitActionLabel('میز خالی شد'),
+            ]);
+    }
+}
