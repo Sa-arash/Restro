abf681c38264fa176dac609b581f4d8fd8dbaa92
saman <138027864+Sa-arash@users.noreply.github.com>
2025-03-13 11:12:07 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::redirect('/', '/home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
